1818da2f30505189e775f34a7adaa892b478a675
dmontoyain <dmontoyainestroza@gmail.com>
2020-03-01 15:01:04 -0400

    changing project structure of common resources

--- NoQuieroMatricularmeConTerminal.Desktop/FormLogin.Designer.cs
+++ NoQuieroMatricularmeConTerminal.Desktop/FormLogin.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
             this.textBoxStudentNo = new System.Windows.Forms.TextBox();
             this.textBoxSSN = new System.Windows.Forms.TextBox();
             this.dateTimePickerBirthDate = new System.Windows.Forms.DateTimePicker();
@@ -45,29 +44,26 @@
             // textBoxStudentNo
             // 
             this.textBoxStudentNo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxStudentNo.Location = new System.Drawing.Point(667, 255);
-            this.textBoxStudentNo.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBoxStudentNo.Location = new System.Drawing.Point(250, 107);
             this.textBoxStudentNo.Name = "textBoxStudentNo";
-            this.textBoxStudentNo.Size = new System.Drawing.Size(841, 71);
+            this.textBoxStudentNo.Size = new System.Drawing.Size(318, 33);
             this.textBoxStudentNo.TabIndex = 0;
             // 
             // textBoxSSN
             // 
             this.textBoxSSN.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSSN.Location = new System.Drawing.Point(667, 589);
-            this.textBoxSSN.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBoxSSN.Location = new System.Drawing.Point(250, 247);
             this.textBoxSSN.Name = "textBoxSSN";
             this.textBoxSSN.PasswordChar = '*';
-            this.textBoxSSN.Size = new System.Drawing.Size(841, 71);
+            this.textBoxSSN.Size = new System.Drawing.Size(318, 33);
             this.textBoxSSN.TabIndex = 1;
             // 
             // dateTimePickerBirthDate
             // 
             this.dateTimePickerBirthDate.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePickerBirthDate.Location = new System.Drawing.Point(667, 422);
-            this.dateTimePickerBirthDate.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.dateTimePickerBirthDate.Location = new System.Drawing.Point(250, 177);
             this.dateTimePickerBirthDate.Name = "dateTimePickerBirthDate";
-            this.dateTimePickerBirthDate.Size = new System.Drawing.Size(841, 71);
+            this.dateTimePickerBirthDate.Size = new System.Drawing.Size(318, 33);
             this.dateTimePickerBirthDate.TabIndex = 2;
             // 
             // label1
@@ -75,10 +71,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(656, 198);
-            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Location = new System.Drawing.Point(246, 83);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(235, 54);
+            this.label1.Size = new System.Drawing.Size(91, 21);
             this.label1.TabIndex = 3;
             this.label1.Text = "Student No.";
             // 
@@ -86,10 +81,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(656, 532);
-            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Location = new System.Drawing.Point(246, 223);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 54);
+            this.label2.Size = new System.Drawing.Size(40, 21);
             this.label2.TabIndex = 4;
             this.label2.Text = "SSN";
             // 
@@ -97,10 +91,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(656, 365);
-            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Location = new System.Drawing.Point(246, 153);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(201, 54);
+            this.label3.Size = new System.Drawing.Size(79, 21);
             this.label3.TabIndex = 5;
             this.label3.Text = "Birth Date";
             // 
@@ -108,21 +101,19 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(656, 699);
-            this.label4.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label4.Location = new System.Drawing.Point(246, 293);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 54);
+            this.label4.Size = new System.Drawing.Size(35, 21);
             this.label4.TabIndex = 7;
             this.label4.Text = "PIN";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(667, 756);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBox1.Location = new System.Drawing.Point(250, 317);
             this.textBox1.Name = "textBox1";
             this.textBox1.PasswordChar = '*';
-            this.textBox1.Size = new System.Drawing.Size(841, 71);
+            this.textBox1.Size = new System.Drawing.Size(318, 33);
             this.textBox1.TabIndex = 6;
             // 
             // label5
@@ -130,10 +121,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(1787, 21);
-            this.label5.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label5.Location = new System.Drawing.Point(670, 9);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(273, 116);
+            this.label5.Size = new System.Drawing.Size(108, 47);
             this.label5.TabIndex = 8;
             this.label5.Text = "Login";
             // 
@@ -142,10 +132,9 @@
             this.ButtonOK.BackColor = System.Drawing.Color.Green;
             this.ButtonOK.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonOK.ForeColor = System.Drawing.Color.White;
-            this.ButtonOK.Location = new System.Drawing.Point(811, 892);
-            this.ButtonOK.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.ButtonOK.Location = new System.Drawing.Point(304, 374);
             this.ButtonOK.Name = "ButtonOK";
-            this.ButtonOK.Size = new System.Drawing.Size(605, 112);
+            this.ButtonOK.Size = new System.Drawing.Size(227, 47);
             this.ButtonOK.TabIndex = 9;
             this.ButtonOK.Text = "OK";
             this.ButtonOK.UseVisualStyleBackColor = false;
@@ -153,20 +142,19 @@
             // 
             // panel1
             // 
-            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImage = global::NoQuieroMatricularmeConTerminal.Desktop.Properties.Resources.upr_rum;
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.Location = new System.Drawing.Point(32, 29);
-            this.panel1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(549, 188);
+            this.panel1.Size = new System.Drawing.Size(206, 79);
             this.panel1.TabIndex = 10;
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(2133, 1073);
+            this.ClientSize = new System.Drawing.Size(810, 466);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.ButtonOK);
             this.Controls.Add(this.label5);
@@ -179,7 +167,6 @@
             this.Controls.Add(this.textBoxSSN);
             this.Controls.Add(this.textBoxStudentNo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "FormLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormLogin";
